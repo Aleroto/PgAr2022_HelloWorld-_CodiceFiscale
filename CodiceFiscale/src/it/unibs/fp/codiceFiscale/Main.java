25acc6bfd5f795324eae09d1883d973b7e288e44
Aleroto <a.rotondi001@studenti.unibs.it>
2022-05-02 22:11:56 +0200

    lettura informazioni

--- CodiceFiscale/src/it/unibs/fp/codiceFiscale/Main.java
+++ CodiceFiscale/src/it/unibs/fp/codiceFiscale/Main.java
@@ -1,12 +1,28 @@
 package it.unibs.fp.codiceFiscale;
 
-public class Main {
+import java.util.ArrayList;
 
+public class Main {
+	
+	
+	
 	public static void main(String[] args) {
 		
-		XML.printXML();
-		//XML.prova2();
+		ArrayList<String> nomi = new ArrayList<String>();
+		XML proova = new XML();
 		
+		
+		System.out.println(proova.peopleReader());
+		proova.peopleReader();		
+		//XML.printXML();
+		
+		
+	}
+	
+	private void print(ArrayList<String> nomi) {
+		for(int i = 0; i<nomi.size();i++) {
+			System.out.println(nomi.get(i));
+		}
 	}
 
 }
--- CodiceFiscale/src/it/unibs/fp/codiceFiscale/XML.java
+++ CodiceFiscale/src/it/unibs/fp/codiceFiscale/XML.java
@@ -8,15 +8,15 @@ import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import java.util.ArrayList;
  
 
 public class XML {
 	private static final String PATH_USER_DIRECTORY = System.getProperty("user.dir");
 	private static final String PATH_XML_INPUT_PERSONE = "/XML/inputPersone.xml";
-	String filePath = PATH_USER_DIRECTORY + PATH_XML_INPUT_PERSONE; //File Path
+	private static final String filePath = PATH_USER_DIRECTORY + PATH_XML_INPUT_PERSONE; //File Path
 	private static final String SEPARATORE = "--------------------------------------------------------------------------------------------------------------------";
-	
-	private FiscalCode fiscalCode;
+
 
 	/*
 	 * Class to support the read and write of GENERAL XML file, try to create a class to read different XML like inputPersone.xml and comuni.xml,
@@ -25,8 +25,8 @@ public class XML {
 
 
 	
-	
-	public void printXML() {		
+	// stampa su terinale l'intero file XML (for development uses)
+	private static void printXML() {		
 		XMLInputFactory xmlInputFactory = null;
 		XMLStreamReader xmlStreamReader  = null;
 		
@@ -65,58 +65,74 @@ public class XML {
 		  }
 	}
 	
-	public int IdBirthplace() {
-		
-	}
-	
-	public String Finder(String idPerson,String objectToFind, String objectType){
+	public ArrayList<String> finder(String type){
 		XMLInputFactory xmlInputFactory = null;
 		XMLStreamReader xmlStreamReader  = null;
-		String object = null;
-		int index = 0;
-		
-		try {
+		ArrayList<String> value = new ArrayList<String>();
+			
+		try {	
 			Reader fileReader = new FileReader(filePath);	//Read XML file.
 			xmlInputFactory = XMLInputFactory.newInstance();	//Get XMLInputFactory instance.			
 			xmlStreamReader  = xmlInputFactory.createXMLStreamReader(fileReader);	//Create XMLStreamReader object.
-			
-			//Iterate through events.
-			while(xmlStreamReader.hasNext()){
+			boolean stato = false;
+			while(xmlStreamReader.hasNext()){			
 			  int xmlEvent = xmlStreamReader.next();	//Get integer value of current event.
-			  switch(xmlEvent) {
-				 case XMLStreamConstants.START_ELEMENT: // inizio di un elemento: stampa il nome del tag e i suoi attributi
-					 for (int i = 0; i < xmlStreamReader.getAttributeCount(); i++) {
-						 separatore();
-						 if(xmlStreamReader.getAttributeLocalName(i) == idPerson) {
-							 index = i;
-						 }
-						 //System.out.printf("=> attributo %s ->%s%n", xmlStreamReader.getAttributeLocalName(i), xmlStreamReader.getAttributeValue(i));
-					 }
-					 break;
-				 case XMLStreamConstants.END_ELEMENT: // fine di un elemento: stampa il nome del tag chiuso
-					 //System.out.println("END-Tag " + xmlStreamReader.getLocalName());
-					 break;
-				 case XMLStreamConstants.COMMENT:
-					 System.out.println("// commento " + xmlStreamReader.getText()); break; // commento: ne stampa il contenuto
-				 case XMLStreamConstants.CHARACTERS: // content all’interno di un elemento: stampa il testo					 
-					 if(xmlStreamReader.getLocalName() == objectToFind){
-						 object = 
-					 }
-					 if (xmlStreamReader.getText().trim().length() > 0) // controlla se il testo non contiene solo spazi
-					 System.out.println("-> " + xmlStreamReader.getText());
-					 break;
+			  if(stato) {
+				  value.add(xmlStreamReader.getText());
+				  stato = false;
+			  }
+			  if(xmlEvent == XMLStreamConstants.START_ELEMENT && xmlStreamReader.getLocalName() == type) {
+				  stato = true;
 			  }
 			}
 		  } catch (Exception e) {
 			e.printStackTrace();
 		  }
-		return object;
+		return value;
 	}
+	
+	private ArrayList<String> nameReader(){
+		ArrayList<String> names = new ArrayList<String>();
+		names = (ArrayList<String>) finder("name").clone();
+		System.out.println(names);
+		return names;
+	}
+	private ArrayList<String> sournameReader(){
+		ArrayList<String> sournames = new ArrayList<String>();
+		return sournames = finder("cognome");
+	}
+	private ArrayList<String> genderReader(){
+		ArrayList<String> genders = new ArrayList<String>();
+		return genders = finder("sesso");
+	}
+	private ArrayList<String> birthPlaceReader(){
+		ArrayList<String> birthPlaces = new ArrayList<String>();
+		return birthPlaces = finder("comune_nascita");
+	}
+	private ArrayList<String> birthDateReader(){
+		ArrayList<String> birthDates = new ArrayList<String>();
+		return birthDates = finder("data_nascita");
+	}
+	
+	public ArrayList<Person> peopleReader() {
+		ArrayList<Person> people = new ArrayList<Person>();
+		for(int i = 0; i < nameReader().size();i++) {
+			people.add(new Person(finder("name").get(i),finder("cognome").get(i),finder("sesso").get(i),null,null,null));
+		}
+		return people;
+	}
+	
+	
 	
 	
 	
 	private static void separatore() {
 		System.out.println(SEPARATORE);
+	}
+	private void print(ArrayList<String> nomi) {
+		for(int i = 0; i<nomi.size();i++) {
+			System.out.println(nomi.get(i));
+		}
 	}
 	
 	
